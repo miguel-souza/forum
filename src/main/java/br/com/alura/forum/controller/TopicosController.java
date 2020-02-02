0da37b7423f4d0a29331b1679fb0e2af51e04e48
yasmin-dev <desouza.yasmin31@gmail.com>
2020-02-01 21:18:11 -0300

    Iniciando projeto

---
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -0,0 +1,34 @@
+package br.com.alura.forum.controller;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.alura.forum.controller.dto.TopicoDto;
+import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.TopicoRepository;
+
+@RestController
+public class TopicosController {
+	
+	@Autowired
+	private TopicoRepository topicoRepository;
+	
+	@RequestMapping("/topicos")
+	public List<TopicoDto> listar(String nomeCurso){
+		if(nomeCurso == null) {
+			List<Topico> topicos = topicoRepository.findAll();
+			return TopicoDto.converter(topicos);
+		}else {
+			List<Topico> topicos = topicoRepository.findByCurso_Nome(nomeCurso);
+			return TopicoDto.converter(topicos);
+		}
+	}
+	
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+
+INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programacao');
+INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
+
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Duvida', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Duvida 2', 'Projeto nao compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Duvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
